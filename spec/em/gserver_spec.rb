b1b44a5a2ee565a480417385d0a61c0ad8ac5079
Marcos Ortiz <marcos.ortiz@icloud.com>
2016-06-09 23:28:57 -0500

    Fixing spec.

--- spec/em/gserver_spec.rb
+++ spec/em/gserver_spec.rb
@@ -1,11 +1,8 @@
 require 'spec_helper'
+require 'em/gserver'
 
-describe Em::Gserver do
-  it 'has a version number' do
-    expect(Em::Gserver::VERSION).not_to be nil
-  end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
+describe EventMachine::GServer do
+    it 'has a version number' do
+        expect(EventMachine::GServer::VERSION).not_to be nil
+    end
 end
